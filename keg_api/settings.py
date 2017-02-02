32cd9994cecfb95fcf7e7a29b2eabf5e59e575f0
Andrew Smiley <andrew.smiley@ge.com>
2017-02-01 21:24:30 -0500

    edits

--- keg_api/settings.py
+++ keg_api/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'keg_api',
-    'predix'
+    'predix',
+    'corsheaders'
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,3 +130,4 @@ STATIC_URL = '/static/'
     #     },
     # },
 # }
+CORS_ORIGIN_ALLOW_ALL = True
